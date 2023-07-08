eb61eef705de91666b622e99cd0bde1d7dc5bd08
Russ <qatsby@yandex.com>
2023-07-07 22:20:28 -0400

    new methods added

--- src/main/java/pom/LoginPage.java
+++ src/main/java/pom/LoginPage.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class LoginPage {
+    private final String baseUrl = "https://www.saucedemo.com/";
+
+    //Xpath locators
     @FindBy(how = How.XPATH, using = ".//input[@name = 'user-name']")
     private SelenideElement usernameField;
     @FindBy(how = How.XPATH, using = ".//input[@name = 'password']")
@@ -13,4 +16,23 @@ public class LoginPage {
     @FindBy(how = How.XPATH, using = ".//input[@name = 'login-button']")
     private SelenideElement loginButton;
 
+    //Method set Username
+    public void setUsername (String username){
+        usernameField.setValue(username);
+    }
+    //Method set Password
+    public void setPassword (String password){
+        passwordField.setValue(password);
+    }
+    //Method click login Button
+    public void clickLoginButton(){
+        loginButton.click();
+    }
+
+    //Method authorization
+    public void login (String username, String password){
+        setUsername(username);
+        setPassword(password);
+        clickLoginButton();
+    }
 }
