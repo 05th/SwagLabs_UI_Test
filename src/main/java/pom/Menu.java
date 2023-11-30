0b784d483c6fe7ab34daa1d2557e0d4eb9988ee3
Russ <qatsby@yandex.com>
2023-11-30 00:17:58 -0500

    added new locators

--- src/main/java/pom/Menu.java
+++ src/main/java/pom/Menu.java
@@ -1,5 +1,14 @@
 package pom;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class Menu {
+ private WebDriver driver;
+ private By closeMenuButton = By.xpath("//button[@id = 'react-burger-cross-btn']");
+ private By allItemsLink = By.xpath("//a[@id = 'inventory_sidebar_link']");
+ private By aboutLink = By.xpath("//a[@id = 'about_sidebar_link']");
+ private By logOutLink = By.xpath("//a[@id = 'logout_sidebar_link']");
+ private By resetAppLink = By.xpath("//a[@id = 'reset_sidebar_link']");
 
 }
