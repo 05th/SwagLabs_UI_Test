1075d2cbb47c1f6154fb4ba97130fa119d9fe89f
Russ <qatsby@yandex.com>
2023-11-30 22:31:29 -0500

    new class

--- src/test/java/HomePageTests.java
+++ src/test/java/HomePageTests.java
@@ -1,4 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pom.HomePage;
+
 public class HomePageTests {
 
+    WebDriver driver = new ChromeDriver();
 
+    HomePage homePage = new HomePage(driver);
 }
